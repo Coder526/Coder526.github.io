13d94445563eb8b5426bc9ea3952214363453e49
Coder526 <ethanjm001@gmail.com>
2019-09-18 15:36:50 +0100

    Add files via upload

---
+++ myfileio.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    
+    int v1;
+    float v2;
+    char v3;
+    
+    v1 = 2;
+    v2 = 4.25;
+    v3 = 'h';
+    /*prints everything out*/
+    printf("v1 contains %d", v1 );
+    
+    printf("v2 contains %f", v2 );
+    
+    printf("v3 contains %c",v3 );
+    return 0;
+}
---
+++ myfileio2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    
+    int v1;
+    float v2;
+    char v3;
+    
+    v1 = 2;
+    v2 = 4.25;
+    v3 = 'h';
+    /*prints everything out*/
+    printf("v1 contains %d\n", v1 );
+    
+    printf("v2 contains %f\n", v2 );
+    
+    printf("v3 contains %c\n",v3 );
+    return 0;
+}
